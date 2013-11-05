955da3fe7bcd7493843197198afa5f9a21b73617
Agnieszka Szefer <agnieszka.m.szefer@gmail.com>
2013-11-05 23:17:52 +0000

    Comments for rewiring.

--- Rewiring.m
+++ Rewiring.m
@@ -1,9 +1,13 @@
+%% Rewiring connections between excitatory-excitatory neurons.
 function Rewiring(probability)
 
 load('Network.mat', 'module');
 
-N = 100;
+N = 100; % Number of excitatory neurons in each module.
 
+% For each existing excitatory-excitatory connection within a module
+% with a given probability create a new intermodular
+% excitatory-excitatory connection.
 for m = 1:8
    for i = 1:N
        for j = 1:N
