5bc34976d43688fcb16734eac718e988ab1d4aa2
matah <selma@apexis.ro>
2013-11-05 17:56:41 +0000

    created rewiring function

--- Rewiring.m
+++ Rewiring.m
@@ -1,19 +1,30 @@
 function Rewiring(probability)
 
-load('Network.mat', 'module', 'inhibitory2excitatory', 'scalingFactorIE', 'weightsIE', 'conductionDelayIE', 'inhibitory2inhibitory', 'scalingFactorII', 'scalingFactorII', 'weightsII', 'scalingFactorEE');
+load('Network.mat', 'module');
 
 N = 100;
 
 for m = 1:8
    for i = 1:N
        for j = 1:N
-           if(module{m}.excitatoryConnectivity(i, j) == 1 && rand < probability)
-               module{m}.excitatoryConnectivity(i, j) = 0;
-               newM = floor(rand*7) + 1;
-               newTarget = floor(rand*99) + 1;
+           if(module{m}.S{m}(i, j) == 1 && rand < probability)
                
-    
+               module{m}.S{m}(i, j) = 0;
+               
+               newM = floor(rand*7) + 1;
+               newNeuron = floor(rand*99) + 1;
+               
+               while(module{m}.S{newM}(newNeuron, i) == 1)
+                   newM = floor(rand*7) + 1;
+                   newNeuron = floor(rand*99) + 1;
+               end
+               
+               module{m}.S{newM}(newNeuron, i) = 1;
+           end
+       end
+   end
 end
 
+save('RewiredNetwork.mat', 'module');
 
 end
